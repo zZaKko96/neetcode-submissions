086598b95c5dee5ec259712db01603d46a938261
ZaKo <139034979+zZaKko96@users.noreply.github.com>
2026-07-31 11:16:14 +0300

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string_view s, string_view t) {
+        if (s.length() != t.length()) return false;
+        int count[26]{0};
+
+        for (size_t i = 0; i < s.length(); i++)
+        {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (int i : count)
+            if (i != 0) return false;
+        return true;
+    }
+};
